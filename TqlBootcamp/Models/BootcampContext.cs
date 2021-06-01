f9aee4a4797383a288c90fa4900ab1893b7900a8
MatteaAnn Swain <MMSwain@tql.com>
2021-06-01 13:36:21 -0400

    Add project files.

---
+++ TqlBootcamp/Models/BootcampContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TqlBootcamp.Models
+{
+// make public and inheriat DbContext 
+    public class BootcampContext : DbContext 
+    {
+// property for table in db 
+        public DbSet<Student> Students { get; set; } // 
+
+ // going to set up sql server connection 
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                var connStr = "server=localhost\\sqlexpress01;database=BootCampDb;trusted_connection=true;";
+                builder.UseSqlServer(connStr);
+            }
+
+        }
+// used for fluid api 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+
+
+
+
+    }
+}
---
+++ TqlBootcamp/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TqlBootcamp.Models
+{
+    public class Student // make class public
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        public string Firstname { get; set; }
+        [Required, StringLength(50)]
+        public string Lastname { get; set; }
+
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal TargetSalary { get; set; }
+
+        public bool? InBootcamp { get; set; } // boolean that allows a null
+
+
+        public Student() { } // default constructer
+
+
+    }
+}
